971ff01fdc53362f0f90eb87acc70c90f5ac67fb
nice <tomaszjanrys@gmail.com>
2020-10-14 19:02:46 +0200

    ...2.... .Java i Kt obiekt wyslany do tablicy  ...

--- src/Uczen.java
+++ src/Uczen.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Uczen {
+public class Uczen {
+    private  String name = "";
+    private  String surname = "";
+    private  Integer myID = 0;
+
+    //Wspolne pole dla wszystkich obiektow okreslamy slowem staic
+    static  Integer id = 0;
+
+
+    Uczen(String name, String surname){
+        myID = id;
+        this.name = name + "_" + myID;
+        this.surname = surname + "_" + myID;
+        myID = id;
+        id++;
+    }
+
+    @Override
+    public String toString() {
+        return "" + name + "" + surname;
+    }
 }
--- src/zajecia2.kt
+++ src/zajecia2.kt
@@ -1,3 +1,15 @@
 fun main(){
-    println("ok")
+    val uczniowie = mutableListOf<Uczen>()
+    //gdy przypiszemy wartosc 100 to numeracja zacznie sie przypisanej wartosci
+     Uczen.id = 100;
+   for (i in 1..11) {
+       uczniowie.add(Uczen("marek", "nowak"));
+   }
+    println(uczniowie);
+    Uczen.id = 0;
+    for(i in 1..11){
+        uczniowie.add(Uczen("WetGoat", "Ninja"))
+    }
+    println("${uczniowie}")
+    println(Uczen.id)
 }
