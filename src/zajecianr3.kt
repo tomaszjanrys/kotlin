40f8b0fd04895fec011d2cb309095276b6640ae1
nice <tomaszjanrys@gmail.com>
2020-10-21 21:25:28 +0200

    zajecia nr3

--- src/zajecianr3.kt
+++ src/zajecianr3.kt
@@ -1,3 +1,9 @@
 fun main(){
-    println("test")
+    val x:Int=55
+    when(x.toInt()){
+        //
+        0 -> println("X wynosi 0")
+        1,2,3,4,5,6 -> println("X nalezy do zbioru {1,2,3,4,5,6}")
+    else -> println("Errr")
+    }
 }
